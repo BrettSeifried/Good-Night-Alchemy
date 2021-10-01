17ed2320a61c9b1f14294a686cfe9ea8fe11aea2
Violet Stewart <vkazoo2@gmail.com>
2021-10-01 16:05:52 -0700

    violets turn driving

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-// import functions and grab DOM elements
-const adjInput1 = document.getElementById('adj-Input1');
+// inputs
+const adjInput1 = document.getElementById('adj1-Input');
 const button = document.getElementById('submit');
 const animalInput = document.getElementById('animal1');
+const animalInput2 = document.getElementById('animal2-Input');
+const noun1 = document.getElementById('noun1-Input');
+const adjective2 = document.getElementById('adj2-Input');
+const noun2 = document.getElementById('noun2-Input');
+//spans
 const add1 = document.getElementById('adj1');
 const add2 = document.getElementById('animal-input');
-
+const add3 = document.getElementById('animal2');
+const add4 = document.getElementById('noun1');
+const add5 = document.getElementById('adj2');
+const add6 = document.getElementById('noun2');
+const add7 = document.getElementById('animal3');
+const add8 = document.getElementById('animal4');
 
 // initialize global state
 button.addEventListener('click', () =>{
