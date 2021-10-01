f13bd04240b4364d7ff4ce607724a5d5fa691ba4
Brett Seifried <seifriedbrett@gmail.com>
2021-10-01 14:58:48 -0700

    added story and first input

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // import functions and grab DOM elements
-
+const adjInput1 = document.getElementById('adjInput1');
+const button = document.getElementById('submit');
+const animalInput = document.getElementById('animal1');
+const inputs = document.getElementById('adj1');
 // initialize global state
+button.addEventListener('click', () =>{
+    inputs.textContent = adjInput1.value;
+    inputs.textContent = animalInput.value;
 
+});
 // set event listeners 
   // get user input
   // use user input to update state 
